4f8616ac22781b80b283d199d5f6f0b0d45a12bc
Navitha N <124995352+Navitha-Nelluri@users.noreply.github.com>
2025-01-21 13:05:43 -0800

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,8 @@
+let weather = prompt(`Please enter rain or sunny: `)
+if(weather == "rain") {
+    console.log(`It is rainy. Please take your umbrilla`)
+}
+else {
+    console.log(`It is Sunny.`)
+}
+
